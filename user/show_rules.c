9261d7b1fa26e6aa7648dbf1ab1a727da1251427
root <plevreuven@gmail.com>
2015-12-14 11:14:46 +0200

    Mon Dec 14 11:14:46 IST 2015

--- user/show_rules.c
+++ user/show_rules.c
@@ -29,7 +29,17 @@ void ip_int_to_string(unsigned int ip, unsigned int mask, char* dst)
 void print_rules(FILE *stream)
 {
 	char rule_name[20];
+	char src_prt_s[20];
+	char dst_prt_s[20];
+	char protocol_s[20];
+	char action_s[20];
+	char ack_s[20];
 	rule_name[0]='\0';
+	src_prt_s[0]='\0';
+	action_s[0]='\0';
+	dst_prt_s[0]='\0';
+	protocol_s[0]='\0';
+	ack_s[0]='\0';
 	unsigned int src_ip;
 	unsigned int src_mask;
 	unsigned int dst_ip;
@@ -42,9 +52,41 @@ void print_rules(FILE *stream)
 	printf("%-20s%-25s%-25s%-10s%-10s%-10s%-10s%-10s\n", "Rule Name", "Source IP", "Dest IP", "Src Port", "Dst Port", "Protocol", "Action", "Ack");
 	while (fscanf(stream, "%s %u %u %u %u %u %u %u %u %u", rule_name, &src_ip, &src_mask, &dst_ip, &dst_mask, &src_prt, &dst_prt, &protocol, &action, &ack) == 10)
 	{
-		ip_int_to_string(src_ip, src_mask, src_ip_mask);
-		ip_int_to_string(dst_ip, dst_mask, dst_ip_mask);
-		printf("%-20s%-25s%-25s%-10u%-10u%-10u%-10u%-10u\n", rule_name, src_ip_mask,  dst_ip_mask, src_prt, dst_prt, protocol, action, ack);
+		if (src_ip == 0)
+			strcpy(src_ip_mask, "ANY");
+		else	
+			ip_int_to_string(src_ip, src_mask, src_ip_mask);
+		if (dst_ip == 0)
+			strcpy(dst_ip_mask, "ANY");	
+		else	
+			ip_int_to_string(dst_ip, dst_mask, dst_ip_mask);
+		if (src_prt == 0)
+			strcpy(src_prt_s, "ANY");
+		else
+			sprintf(src_prt_s, "%u", src_prt);
+		if (dst_prt == 0)
+			strcpy(dst_prt_s, "ANY");
+		else
+			sprintf(dst_prt_s, "%u", dst_prt);
+		if (protocol == 143)
+			strcpy(protocol_s, "ANY");
+		else if (protocol == 17)
+			strcpy(protocol_s, "UDP");	
+		else if (protocol == 6)
+			strcpy(protocol_s, "TCP");	
+		else
+			strcpy(protocol_s, "OTHER");
+		if (action == 1)
+			strcpy(action_s, "ACCPET");
+		else
+			strcpy(action_s, "DROP");
+		if (ack == 3)
+			strcpy(ack_s, "ANY");
+		else if (ack == 1)
+			strcpy(ack_s, "NO");
+		else
+			strcpy(ack_s, "YES");
+		printf("%-20s%-25s%-25s%-10s%-10s%-10s%-10s%-10s\n", rule_name, src_ip_mask,  dst_ip_mask, src_prt_s, dst_prt_s, protocol_s, action_s, ack_s);
 	}
 }
 
